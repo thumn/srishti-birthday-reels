4ba9bfad8c778a712267a517c26f5159c45dbffc
Thu Nguyen <tmnguyen@berkeley.edu>
2026-04-28 23:50:08 -0700

    fixed video item import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import VideoItem from './VideoItem';
+import VideoItem from './components/video_item';
 import videoData from './videos.json';
 
 function App() {
